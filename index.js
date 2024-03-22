58eeb219cc0ae84b973100be268faf18428718ba
MaxAnii <ansarbaba2000@gmail.com>
2024-03-22 23:43:41 +0530

    added a function, in response to message.

--- index.js
+++ index.js
@@ -5,18 +5,29 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const client = require("./utils/whatsapp-web");
 const router = require("./routers/send-otp");
+const db = require("./utils/db");
 const corsOptions = {
 	origin: "http://localhost:3000",
 	methods: "GET,POST",
 };
 
 app.use(cors(corsOptions));
-client.initialize();
-
 app.use(bodyParser.json());
 
 app.use("/", router);
-
+client.on("message", async (msg) => {
+	const chat = await msg.getChat();
+	const test = {
+		Name: chat.name,
+		Description: chat.description,
+		Created_At: chat.createdAt.toString(),
+		Created_By: chat.owner.user,
+		participants: chat.participants,
+		Participant_count: chat.participants.length,
+	};
+	console.log(test);
+});
+client.initialize();
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
