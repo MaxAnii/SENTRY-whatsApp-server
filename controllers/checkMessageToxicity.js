23a8ec5f1bc8065e820f081a5ad7accf85fa60ea
MaxAnii <ansarbaba2000@gmail.com>
2024-03-26 15:47:32 +0530

    Added function to check the message is toxic or not.

---
+++ controllers/checkMessageToxicity.js
@@ -0,0 +1,7 @@
+const checkMessageToxicity = async (message, toltoleranceLevel) => {
+	console.log(message, toltoleranceLevel);
+	// pass the message to the model
+	return true;
+};
+
+module.exports = checkMessageToxicity;
