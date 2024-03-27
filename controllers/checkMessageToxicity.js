24eb9965b4c755f33bb7fd6fcfdf3910069ffbc3
MaxAnii <ansarbaba2000@gmail.com>
2024-03-27 13:57:15 +0530

    Connect to model to check toxicity of message.

--- controllers/checkMessageToxicity.js
+++ controllers/checkMessageToxicity.js
@@ -1,7 +1,22 @@
-const checkMessageToxicity = async (message, toltoleranceLevel) => {
-	console.log(message, toltoleranceLevel);
-	// pass the message to the model
-	return true;
+require("dotenv").config();
+const OpenAI = require("openai");
+const prompt = require("../utils/promptTemplate");
+const openai = new OpenAI({
+	apiKey: process.env.OpenAI,
+});
+const checkMessageToxicity = async (message, toleranceLevel) => {
+	const completion = await openai.chat.completions.create({
+		messages: [
+			{
+				role: "system",
+				content: prompt(message),
+			},
+		],
+		model: "gpt-3.5-turbo",
+	});
+	const score = completion.choices[0].message.content.slice(-1);
+	console.log("chatgpt score__", score);
+	return score >= toleranceLevel;
 };
 
 module.exports = checkMessageToxicity;
