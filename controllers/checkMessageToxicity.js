2d1826c2bcd3360ff792b2a7f2d167a7c3f63baa
MaxAnii <ansarbaba2000@gmail.com>
2024-03-28 13:51:16 +0530

    remove the console statement

--- controllers/checkMessageToxicity.js
+++ controllers/checkMessageToxicity.js
@@ -15,7 +15,6 @@ const checkMessageToxicity = async (message, toleranceLevel) => {
 		model: "gpt-3.5-turbo",
 	});
 	const score = completion.choices[0].message.content.slice(-1);
-	console.log("chatgpt score__", score);
 	return score >= toleranceLevel;
 };
 
