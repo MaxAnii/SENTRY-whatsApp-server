ae5403ce9ff1841b2b98681e27e9e6b4aca8410d
MaxAnii <ansarbaba2000@gmail.com>
2024-03-28 13:50:56 +0530

    Update the warning message content.

--- controllers/sendWarningMessage.js
+++ controllers/sendWarningMessage.js
@@ -3,11 +3,17 @@ const client = require("../utils/whatsapp-web");
 const sendMessage = async (user) => {
 	console.log("sending message", user);
 	const userPhoneNumber = user + "@c.us";
-	await client.sendMessage(userPhoneNumber, "this is your warning message");
+	await client.sendMessage(
+		userPhoneNumber,
+		"Your recent actions violate our group's policies. Please refrain from such behavior to maintain a respectful environment. Further violations may lead to removal from the group."
+	);
 };
 const sendLastMessage = async (user) => {
 	const userPhoneNumber = user + "@c.us";
-	await client.sendMessage(userPhoneNumber, "your are removed from the group");
+	await client.sendMessage(
+		userPhoneNumber,
+		"We regret to inform you that due to repeated violations of our group policies, you have been removed from the group. We value respectful conduct among members, and unfortunately, your actions did not align with our community standards."
+	);
 };
 
 module.exports = { sendMessage, sendLastMessage };
