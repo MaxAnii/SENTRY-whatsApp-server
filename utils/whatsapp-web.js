bb5fb593df25045db1482dbc5167d6b72e223fa4
MaxAnii <ansarbaba2000@gmail.com>
2024-04-06 21:25:03 +0530

    Change the session path.

--- utils/whatsapp-web.js
+++ utils/whatsapp-web.js
@@ -3,7 +3,12 @@ const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 
 const client = new Client({
-	authStrategy: new LocalAuth(),
+	authStrategy: new LocalAuth({ dataPath: "sessions" }),
+	webVersionCache: {
+		type: "remote",
+		remotePath:
+			"https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+	},
 });
 
 client.on("ready", () => {
